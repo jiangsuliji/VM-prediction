b8f9ad96c04d72a1da54936137b2ff5db7215ce5
jiangsuliji <jiangsuliji@gmail.com>
2018-03-30 15:06:01 -0700

    server farm task model added

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ if len(CPUtrace0) != len(MEMtrace0): print("ERROR0: len of CPU and MEM trace dif
 price = [0.12,0.156274943634288,0.165628818358787,0.117693804804409,0.194992254682768,0.192264958784603,0.318664473877846,0.266636511990327,0.326670564838889,0.293872217772597,0.388948689571630,0.359970142368528,0.447810550493127,0.478695373133980,0.513388465378109,0.491034024181452,0.457621946795299,0.506109999205217,0.640870819945530,0.544380483035076,0.592130716398328,0.486134789641609,0.499381554660302,0.292508084600509]
 
 # generate server cluster - server[x][t][CPU,MEM] 20 servers
-server = [[[0,0] for _ in range(24)] for _ in range(20)]
+server_num = 20
+server = [[[0,0] for _ in range(24)] for _ in range(server_num)]
 server_cpu = 100000 # upper bound CPU
 server_mem = 100000 # upper bound MEM
 #print(server)
@@ -50,6 +51,14 @@ task0 = generate_task(CPUtrace0,MEMtrace0)
 task1 = generate_task(CPUtrace1,MEMtrace1)
 
 print(task0)
+
+# negotiation algorithm - first try intra only - TODO add inter later
+iteration = 5
+a = 0.1 # intra congestion term
+b = 0.1 # inter congestion term
+cserver_intra = [[0 for _ in range(24)] for _ in range(server_num)]
+
+print(cserver_intra)
 
 
 
